c5d1392722e2fd6f5bc65078cc85a3231477b720
hmiyamoto0130 <h.miyamoto@menew.jp>
2023-02-05 12:41:22 +0900

    DB対応

--- app.py
+++ app.py
@@ -1,6 +1,73 @@
+from functools import lru_cache
+
 from flask import Flask, render_template, request
 from dataclasses import dataclass
 from datetime import datetime
+import sqlalchemy
+# databaseに接続（postgres）
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.types import Integer, String, DateTime
+Base = declarative_base()
+app = Flask(__name__)
+engine = create_engine('postgresql://hiroyuki:DBNi76tCKqjfNBcxCJvIZQhWqC8uW2Cd@dpg-cffg9m82i3mg6p8memu0-a.singapore-postgres.render.com:5432/test')
+
+from sqlalchemy.schema import Column
+from sqlalchemy.orm import sessionmaker
+SessionClass = sessionmaker(engine)  # セッションを作るクラスを作成
+session = SessionClass()
+
+# スキーマをtestに変更
+# engine.execute("SET search_path TO test")
+
+# userの情報を取得
+class User(Base):
+    __tablename__ = 'user'
+    __table_args__ = {'schema': 'test'}
+    id = Column(Integer, primary_key=True)
+    name = Column(String(80), unique=False, nullable=False)
+    def __repr__(self):
+        return f'<User id={self.id}, name={self.name}>'
+
+# 全userの情報を取得する関数
+def get_all_users():
+    return session.query(User).all()
+
+# userを登録する関数
+def add_user(name):
+    user = User(name=name)
+    session.add(user)
+    session.commit()
+
+# 送金情報を取得
+class SendMoney(Base):
+    __tablename__ = 'send_money'
+    __table_args__ = {'schema': 'test'}
+    id = Column(Integer, primary_key=True)
+    from_user_id = Column(Integer, unique=False, nullable=False)
+    to_user_id = Column(Integer, unique=False, nullable=False)
+    money = Column(Integer, unique=False, nullable=False)
+    date = Column(DateTime, unique=False, nullable=False)
+
+    def __repr__(self):
+        return f'<SendEvent id={self.id}, from_user_id={self.from_user_id}, to_user_id={self.to_user_id}, money={self.money}, date={self.date}>'
+
+@lru_cache()
+def get_id2name():
+    return {user.id:user.name for user in get_all_users()}
+
+@lru_cache()
+def get_name2id():
+    return {user.name:user.id for user in get_all_users()}
+
+def add_send_money(from_user_id,to_user_id,money):
+    date = datetime.now()
+    send_money = SendMoney(from_user_id=from_user_id,to_user_id=to_user_id,money=money,date=date)
+    session.add(send_money)
+    session.commit()
+
+def get_all_send_money():
+    return session.query(SendMoney).all()
 
 @dataclass
 class SendEvent:
@@ -14,8 +81,9 @@ class SendEvent:
 # app = Flask(__name__)
 app = Flask(__name__,template_folder="./",static_folder="./",static_url_path="/")
 # data_list = [SendEvent() for i in range(10)]
-data_list = []
+data_list = [SendEvent(from_user_name=get_id2name()[data.from_user_id],to_user_name=get_id2name()[data.to_user_id],money=data.money,date=data.date) for data in get_all_send_money()]
 
+print(data_list)
 
 @app.route('/')
 def hello_world():  # put application's code here
@@ -33,6 +101,20 @@ def send():  # put application's code here
             )
         )
         print(data_list)
+        add_send_money(
+            from_user_id=get_name2id()[request.args.get("from_user_name")],
+            to_user_id=get_name2id()[request.args.get("to_user_name")],
+            money=int(request.args.get("money"))
+        )
+    else:
+        # 既存ユーザの一覧になかったらuserに追加
+        if request.args.get("from_user_name") not in get_name2id().keys():
+            add_user(request.args.get("from_user_name"))
+            get_name2id.cache_clear()
+            get_id2name.cache_clear()
+            get_id2name()
+            get_name2id()
+
     return render_template("send.html",user_name=request.args.get("from_user_name"))
 
 @app.get('/summary')
@@ -49,4 +131,13 @@ def get_summary():  # put application's code here
 
 
 if __name__ == '__main__':
+    # add_user("kiyona")
+    # inspector = sqlalchemy.inspect(engine)
+    # db内のテーブル一覧を表示
+    # print(inspector.get_table_names())
+    # columns = inspector.get_columns("user")
+    # print(get_all_users())
+    # add_send_money(1,2,100)
+    # add_send_money(2,1,100)
+    # print(session.query(SendMoney).all())
     app.run(port=5000, debug=True)
